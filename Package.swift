793f57b4edeab58dc609adfa9fea70bb0a43e064
Nick Brook <nrbrook@gmail.com>
2021-03-14 20:24:21 +0000

    4.4.0-b.2

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let checksum = "f6d9caa96be5d59a2567d50143935e3c99939141bb05206243239a26cff812c8
 
 let package = Package(
     name: "AirTurnInterface",
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
